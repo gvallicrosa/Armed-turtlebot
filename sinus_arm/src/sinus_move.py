c70f431ba728ebb79678e03a38516c24737dc870
GVallicrosa <guillemvallicrosa@gmail.com>
2012-09-20 15:20:03 +0100

    New random generator. Showed to Yvan.

--- sinus_arm/src/sinus_move.py
+++ sinus_arm/src/sinus_move.py
@@ -14,6 +14,7 @@ from dynamixel_msgs.msg import JointState, MotorStateList
 # Maths and sleep
 from numpy import sin, pi, arange
 from time import sleep
+import random
 
 
 
@@ -23,6 +24,9 @@ from time import sleep
 #def sub_wrist_roll(msg):
 #def sub_shoulder_pan(msg):
 #def sub_shoulder_pitch(msg):
+
+
+
 ################################################################################
 def main():
     # Initialize ROS node
@@ -38,10 +42,15 @@ def main():
     
     # Sinus movement
     for t in arange(1e3):
-        val = sin(2*pi*t/100)
+#        val = 0.5*sin(2*pi*t/100)
+        val = (random.random()*1.7 -1)
         pub_wrist_roll.publish(Float64(val))
         pub_grasp.publish(Float64(val))
-        sleep(0.05)
+        pub_elbow_flex.publish(Float64(val))
+        pub_wrist_roll.publish(Float64(val))
+        pub_shoulder_pan.publish(Float64(val))
+        pub_shoulder_pitch.publish(Float64(val))
+        sleep(2) # enough time to move to the random position
     
     # Arm subscribers (to check joint load, velocities...)
 #    rospy.Subscriber('/right_finger_controller/state',   JointState, sub_grasp)
@@ -49,6 +58,7 @@ def main():
 #    rospy.Subscriber('/wrist_roll_controller/state',     JointState, sub_wrist_roll)
 #    rospy.Subscriber('/shoulder_pan_controller/state',   JointState, sub_shoulder_pan)
 #    rospy.Subscriber('/shoulder_pitch_controller/state', JointState, sub_shoulder_pitch)
+
 
 
 
