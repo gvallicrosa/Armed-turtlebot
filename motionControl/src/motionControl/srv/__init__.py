5baa963d9f18684a505b563bb37155a3b8a62026
james@agentagile.com <iceaxe@outlook.com>
2012-11-18 23:39:14 +0000

    housekeeping

--- motionControl/src/motionControl/srv/__init__.py
+++ motionControl/src/motionControl/srv/__init__.py
@@ -1,7 +1,2 @@
-from ._motionControl_stop import *
-from ._motionControl_backward import *
-from ._motionControl_left import *
 from ._motionControl_timedMove import *
-from ._motionControl_forward import *
-from ._motionControl_right import *
 from ._motionControl_move import *
