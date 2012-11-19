7cc85c890d2ade572866fa2c54aea2c1309f6d14
iceaxe@outlook.com <james@agentagile.com>
2012-11-19 15:21:16 +0000

    task: handleCrash complete

--- mc/src/tasks/handleCrash.py
+++ mc/src/tasks/handleCrash.py
@@ -9,6 +9,8 @@ import rospy
 
 # Custom modules
 from task import task
+from mc.srv import motionControl_move
+from mc.srv import motionControl_timedMove
 
 ###############################################################################
 
@@ -20,4 +22,24 @@ class handleCrash(task):
     def task(self, statusServices=[]):
         """Handle crash scenario."""
         rospy.loginfo("* Handling crash...")
+
+        # 1. Reverse away from obstacle.
+        self.requestService(motionControl_timedMove, (-0.1, 0.0, 3))
+
+        # Stop
+        self.requestService(motionControl_move, (0.0, 0.0))
+
+        # 2. Turn away from obstacle.
+        self.requestService(motionControl_timedMove, (0.0, 0.1, 3))
+
+        # Stop
+        self.requestService(motionControl_move, (0.0, 0.0))
+
+        # 3. Move forwards.
+        self.requestService(motionControl_timedMove, (0.1, 0.0, 3))
+
+        # Stop
+        self.requestService(motionControl_move, (0.0, 0.0))
+
+        # NOTE: The turtlebot will need to relocate the target.
         self.requestService(mc_updateBelief, ("crashed", 0))
