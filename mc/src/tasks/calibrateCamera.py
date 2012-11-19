aeade9bcf35801529c9b20f8f7064f829d080eb0
iceaxe@outlook.com <james@agentagile.com>
2012-11-19 14:54:59 +0000

    task: calibrate camera complete

--- mc/src/tasks/calibrateCamera.py
+++ mc/src/tasks/calibrateCamera.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Python modules
+import os
 
 # ROS modules
 import roslib
@@ -21,4 +22,19 @@ class calibrateCamera(task):
 
     def task(self, statusServices=[]):
         """Calibrate the camera."""
+
+        #######################################################################
+        #                 DO NOT EDIT CODE ABOVE THIS LINE                    #
+        #######################################################################
+
+        # CHANGE THE FOLLOWING TWO VARIABLES AS NECESSARY
+        command = "./calibrateInt -p"
+        image = "/home/user/calibData/img-%02d.pgm"
+
+        #######################################################################
+        #                 DO NOT EDIT OCDE BELOW THIS LINE                    #
+        #######################################################################
+
+        result = os.system(command + image)
+        rospy.loginfo("calibrateCamera: Camera calibrates.")
         self.requestService(mc_updateBelief, ("cameraCalibrated", 1))
