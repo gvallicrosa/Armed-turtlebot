b20cbbf5c3d886d003bbf5006ada92fc14ccef0d
iceaxe@outlook.com <james@agentagile.com>
2012-11-19 14:32:11 +0000

    tasks.graspTarget complete

--- mc/src/tasks/graspTarget.py
+++ mc/src/tasks/graspTarget.py
@@ -10,6 +10,7 @@ import rospy
 # Custom modules
 from task import task
 from mc.srv import mc_updateBelief
+from mc.srv import vs_graspTarget
 ###############################################################################
 
 class graspTarget(task):
@@ -17,8 +18,25 @@ class graspTarget(task):
     name = "graspTarget"
 
     def __init__(self):
-        pass
+        # Assume that we can't see the target.
+        self.grasped = 0
+
+    ###########################################################################
+
+    def targetGrasped(self, msg):
+        self.grasped = msg
+
+    ###########################################################################
 
     def task(self, statusServices=[]):
         """Grasp the target."""
-        self.requestService(mc_updateBelief, ("targetGrasped", 1))
+
+        # Listen if the target has been located. (y/n)
+        # This topic must be published by the camera node.
+        rospy.Subscriber('vs_targetGrasped', Int8, self.targetGrasped)
+
+        # Start the visual servoing.
+        self.requestService(vs_graspTarget)
+
+        # Tell mission control the target has been grasped.
+        self.requestService(mc_updateBelief, ("targetGrasped", grasped))
