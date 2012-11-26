ed14d6a9a988e10c1fd3b8ad5211f80d567b2ce5
marc <marc@MarcVaio.(none)>
2012-11-26 16:37:48 +0000

    Ball detection final program

--- tracking/src/detection.cpp
+++ tracking/src/detection.cpp
@@ -1,16 +1,13 @@
 //Includes all the headers necessary to use the most common public pieces of the ROS system.
 #include <ros/ros.h>
-//Use image_transport for publishing and subscribing to images in ROS
 #include <image_transport/image_transport.h>
-//Use cv_bridge to convert between ROS and OpenCV Image formats
 #include <cv_bridge/cv_bridge.h>
-//Include some useful constants for image encoding. Refer to: http://www.ros.org/doc/api/sensor_msgs/html/namespacesensor__msgs_1_1image__encodings.html for more info.
 #include <sensor_msgs/image_encodings.h>
-//Include headers for OpenCV Image processing
 #include <opencv2/imgproc/imgproc.hpp>
-//Include headers for OpenCV GUI handling
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <std_msgs/String.h>
+#include <sstream>
 
 //Store all constants for image encodings in the enc namespace to be used later.
 namespace enc = sensor_msgs::image_encodings;
@@ -24,12 +21,15 @@ bool Never_detected = true;
 //Declare a string with the name of the window that we will create using OpenCV where processed images will be displayed.
 static const char WINDOW[] = "Image Processed";
 
-//Use method of ImageTransport to create image publisher
-image_transport::Publisher pub;
+//Declare publisher of type string
+
+ros::Publisher pub1;
+ros::Publisher pub2;
 
 //This function is called everytime a new image is published
 void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 {
+    // OUTPUT PARAMETERS PUBLISHED
 	cv::Point centroid = cv::Point(-1,-1);
 	double circ_radius=-1;
 	vector< cv::Point > cont_points;
@@ -38,9 +38,13 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 	int structE_size = 7;
 
 	// PARAMETERS TO SET marc_vaio --- big blue ball
-	/*	double low_sat=50, high_sat =150;
-		double low_hue=110, high_hue = 135;
-	*/
+	//	double low_sat=50, high_sat =150;
+	//	double low_hue=110, high_hue = 135;
+	
+	// Debug
+	// 0 - No debug
+	// 1 - Show windows
+	// 2 - Show windows + set threshold parameters 
 
 	// parameters to set C310 webcam---big blue ball
 	/*double low_hue = 20, high_hue = 50; // 20 -50	
@@ -62,8 +66,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 	
 	try
 	{
-		//Always copy, returning a mutable CvImage
-		//OpenCV expects color images to use BGR channel order.
 		cv_ptr = cv_bridge::toCvCopy(original_image, enc::BGR8);
 	}
 	catch (cv_bridge::Exception& e)
@@ -76,18 +78,16 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 	cv::Mat imWebcam,imHSV, mask;
 	cv::GaussianBlur(cv_ptr->image, imWebcam,cv::Size(11,11),11);
 	cv::cvtColor( imWebcam, imHSV, CV_BGR2HSV );
-
 	min_dist = imWebcam.rows/10;
 	
-	// MANUAL INPUT OF THE THRESHOLDS
-	/*cout<< "enter low and high hue"<<endl;
-	cin>>low_hue;
-	cin>>high_hue;
-	cout<< "enter low and high saturation"<<endl;
-	cin>>low_sat;
-	cin>>high_sat;
-	cout<< "-------------"<<endl<<endl;
-	*/
+	if ( debug == 2){
+	    cout << " enter low and high hue thresholds [0 - 255] : " << endl;
+	    cin>>low_hue;
+	    cin>>high_hue;
+	    cout<< "enter low and high saturation thresholds [0 255]"<<endl;
+	    cin>>low_sat;
+	    cin>>high_sat;
+	}
 	cv::inRange(imHSV,cv::Scalar(low_hue,low_sat,0),cv::Scalar(high_hue,high_sat,255),mask);
 	
 
@@ -99,7 +99,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 	cv::drawContours(mask2, contours, -1, cv::Scalar::all(255), CV_FILLED);
 	
 	// ERODE + DILATE
-
 	cv::Mat str_elem = getStructuringElement(cv::MORPH_ELLIPSE,
                                        cv::Size( 2*structE_size + 1, 2*structE_size+1 ),
                                        cv::Point( structE_size, structE_size ) );
@@ -124,7 +123,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 	if(circles.size()!=0){ // the first circle is the most voted
 		cv::Point center(cvRound(circles[0][0]), cvRound(circles[0][1]));
          	int radius = cvRound(circles[0][2]);
-         	// draw the circle outline
          	cv::circle( imWebcam, center, radius, cv::Scalar(0,0,255), 1, 1, 0 );
 		
 		// Create ROI near detected hough Circle
@@ -132,11 +130,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 		offset2 = cv::Point (radius*roi_fact*2, radius*roi_fact*2); 		
 
 		//Check Roi boundaries
-		//cout << "Image size = " << mask2.rows << " , " << mask2.cols <<endl;
-		//cout<< " BEFORE BOUNDARIES top right : "<< offset.x << " , " <<offset.y <<endl;
-		//cout << "bottom left : " << offset2.x + offset.x << " , " << offset2.y + offset.y <<endl<<endl;
-		
-
 		if( offset.x <0 || offset.x >= mask2.cols){ 
 			offset.x = 0;		
 		}
@@ -167,8 +160,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 		
 		cv::Mat im_roi = mask2(roi);
 		im_roi = im_roi.clone();
-		cv::namedWindow( "ROI",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
-		cv::imshow("ROI",im_roi);
+		
+		if (debug == 1 || debug == 2){
+		    cv::namedWindow( "Region of Interest",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
+	        cv::imshow("Region of Interest",im_roi); 
+		}
 		
 		// Obtain the contour of the blob in ROI
 		cv::findContours(im_roi,contours,CV_RETR_LIST,CV_CHAIN_APPROX_NONE,offset); 
@@ -206,15 +202,14 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 		}
 	}
 	
-	//cv::imshow("WIND_MASK",mask);
+	// Create output strings
+	// message 1 - ball centroid + radius
 	char str1[1024], str2[1024];
-	sprintf( str1, "%d %d %.3f ", centroid.x, centroid.y, circ_radius );
-	
+	sprintf( str1, "%d %d %.3f ", centroid.x, centroid.y, circ_radius );	
 	string message1 = string(str1);
-	cout<< message1<<endl;
 
+    // message 2 - Contour points
 	string message2 = "";
-
 	if (cont_points.size()!=0 ){
 		for (int i=0; i<5;i++){
 			sprintf(str2, "%d %d ", cont_points[i].x, cont_points[i].y );
@@ -222,29 +217,27 @@ void imageCallback(const sensor_msgs::ImageConstPtr& original_image)
 		}
 	}
 		
-	cout << message2<<endl;
 	
-	// DISPLAY IMAGES
-	cv::namedWindow( "New_mask",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
-	cv::namedWindow( "Filled_mask",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
-	cv::namedWindow( "Original",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
+	// Display images 
+	if ( debug==1 || debug == 2){	
+	    cv::namedWindow( "Segmentation",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
+	    cv::namedWindow( "Filtered segmentation",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );
+	    cv::namedWindow( "Original image",  CV_WINDOW_NORMAL| CV_GUI_EXPANDED  );	
+	       
+	    cv::imshow("Segmentation",mask);
+	    cv::imshow("Filtered segmentation",mask2);
+	    cv::imshow("Original image",imWebcam);
+	}
 	
-	cv::imshow("Original",imWebcam);
-	cv::imshow("New_mask",mask);
-	cv::imshow("Filled_mask",mask2);
-
-	cv::waitKey(3);
+	// Publish message1 and message 2	
+	std_msgs::String msg1, msg2;
+    msg1.data = message1;
+    msg1.data = message2;
+    	
+	pub1.publish(msg1);
+	pub2.publish(msg2);
 	
-	/**
-	* The publish() function is how you send messages. The parameter
-	* is the message object. The type of this object must agree with the type
-	* given as a template parameter to the advertise<>() call, as was done
-	* in the constructor in main().
-	*/
-	//Convert the CvImage to a ROS image message and publish it on the "camera/image_processed" topic.
-	cv_ptr->image = imWebcam;
-        pub.publish(cv_ptr->toImageMsg());
-	//prev = vec_hsv[2];
+    cv::waitKey(1);
 }
 
 
@@ -254,7 +247,9 @@ int main(int argc, char **argv)
 {
 	// Init ROS node
     ros::init(argc, argv, "ball_detector");
-	ros::NodeHandle nh;
+    ros::NodeHandle nh;	
+	pub1 = nh.advertise<std_msgs::String>("/detection/message1", 2);
+    pub2 = nh.advertise<std_msgs::String>("/detection/message2", 2);
     
     // Obtain the parameters from server
     nh.getParam("/detection/low_hue",  low_hue);
@@ -263,18 +258,18 @@ int main(int argc, char **argv)
     nh.getParam("/detection/high_sat", high_sat);
     nh.getParam("/detection/debug",    debug);
     ROS_INFO("Node: ball_detector initialized");
+    
+    debug=1;
 
 	//Create an ImageTransport instance, initializing it with our NodeHandle.
     image_transport::ImageTransport it(nh);
 	
 	//OpenCV HighGUI call to create a display window on start-up.
-	cv::namedWindow(WINDOW, CV_WINDOW_AUTOSIZE);
-	
     image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
 	
     //OpenCV HighGUI call to destroy a display window on shut-down.
-	cv::destroyWindow(WINDOW);
-	pub = it.advertise("camera/image_processed", 1);
+	//pub = it.advertise("/detection/image_hue", 1);
+	
     // Continue execution forever
     ros::spin();
 }
