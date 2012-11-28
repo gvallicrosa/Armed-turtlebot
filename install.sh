02a11a3ac4d0069725b2090e532be4e4d58bcb59
Federico <fede@fedeLinux.(none)>
2012-11-28 20:00:32 +0000

    Install xterm on the script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # DEPENDENCIES
 ## Install dependencies
-sudo apt-get install pyqt4-dev-tools python-qt4 python-qt4-dev qt4-dev-tools python-rosdep ros-fuerte-dynamixel-motor
+sudo apt-get install pyqt4-dev-tools python-qt4 python-qt4-dev qt4-dev-tools python-rosdep ros-fuerte-dynamixel-motor xterm
 
 # SMART_ARM_CONTROLLER
 ## Move to ros folder and download the smart_arm_controller
@@ -10,9 +10,11 @@ cd ..
 svn checkout http://ua-ros-pkg.googlecode.com/svn/trunk/arrg/crustcrawler_smart_arm/smart_arm_controller
 rosdep install smart_arm_controller
 rosmake smart_arm_controller
+
 ## Patch the controller configuration
 cp Armed-turtlebot/smart_arm.yaml smart_arm_controller/config/smart_arm.yaml
 cp Armed-turtlebot/smart_arm.launch smart_arm_controller/launch/smart_arm.launch
+
 ## Change permissions to access the arm
 echo 'SUBSYSTEMS=="usb", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", GROUP="plugdev", MODE="0777", SYMLINK+="smart_arm", MODE="0666", GROUP="plugdev"' > temp 
 sudo mv temp /etc/udev/rules.d/88-smart_arm.rules
